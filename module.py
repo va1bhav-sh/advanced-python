7129ac14f789cdbd1f2af89ea1ae749136d2191a
va1bhav-sh <vaibhavsharma.dev28@gmail.com>
2026-03-24 11:26:21 +0530

    main

---
+++ module.py
@@ -0,0 +1,11 @@
+def myFunc():
+    print("Hello Worldd!")
+    
+
+print(__name__)
+
+if __name__ == "__main__":
+    #IF this code is directly executed by running the file its present in
+    print("we are directly running this code")
+    
+print(__name__)
